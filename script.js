c0ad285ea5df4b53c8dc8de264a02f6772c3b6b3
remopou <71232027+remopou@users.noreply.github.com>
2021-05-29 18:20:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function compute()
     var year = new Date().getFullYear() + parseInt(years);
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>";
 } else{
+    alert("Please Enter Positive Number");
     document.getElementById("principal").focus();
 }
 }
